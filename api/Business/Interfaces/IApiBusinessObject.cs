062f9929dd7403de84923521294b5e8108bb0d90
jocko <pedro.silva@eqsglobal.com>
2023-10-18 16:01:45 +0100

    retrieve api version

---
+++ api/Business/Interfaces/IApiBusinessObject.cs
@@ -0,0 +1,16 @@
+﻿using Business.Base;
+using Business.BusinessModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Interfaces
+{
+    public interface IApiBusinessObject
+    {
+        public Task<OperationResult<VersionInfoBusinessModel>> GetApiVersion();
+
+    }
+}
